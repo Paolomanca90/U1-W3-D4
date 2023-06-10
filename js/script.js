3eb2e71be84820d105105e9abdbce5be5b00144a
Paolomanca90 <paolomanca1990@gmail.com>
2023-06-10 14:35:20 +0200

    second fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// genero il tabellone
+
 const tabellone = document.getElementById('tabellone')
 
 const generateTable = function(){
@@ -15,13 +17,73 @@ for(let i=0; i<76; i++){
 
 generateTable()
 
+// funzione per i numeri casuali
+
 const randomNumber = function(){
     const num = Math.floor(Math.random()*76)+1
     return num
 }
 
+// genero le cartelle
+
+// numeri per le cartelle
+let arrCards = []
+
+const numInCard = function(){  
+    for(let i=0; i<24; i++){
+    const randomN = function(){
+        let random = randomNumber()
+        if(arrCards.includes(random)){
+            randomN()
+        }else{
+            arrCards.push(random)
+            }
+        }
+        randomN()
+    } 
+    return arrCards
+}
+
+
+// cartelle
+const addCards = document.querySelector('section')
+const newCard = document.getElementById('new-card')
+
+const generateCard = function (){ 
+    // for(let y=0; y<playerCard; y++){
+     for(let i=0; i<24; i++){
+        const numCell = document.createElement('p')
+        addCards.appendChild(numCell)
+     }
+    
+    }
+
+//  }
+
+newCard.addEventListener('click', function(e){
+    generateCard()
+    let divInSection = document.querySelectorAll('section>p')
+    const newDiv = document.createElement('div')
+    
+    numInCard()
+    for(let i=0; i<24; i++){
+        divInSection[i].innerText = arrCards[i]
+    }
+    divInSection.forEach((el)=>{
+        newDiv.appendChild(el)
+    })
+    addCards.appendChild(newDiv)
+    newDiv.classList.add('new-card')
+    arrCards = []
+})
+
+// estraggo i numeri
+
 const button = document.querySelector('button')
 const cell = document.querySelectorAll('#tabellone div')
+const allP = document.querySelectorAll('section p')
+console.log(allP)
+
 let arr =[]
 
 const randomF = function(){
@@ -34,53 +96,13 @@ const randomF = function(){
         if(Number(p.innerText) === random ){
             p.classList.add('color')
             }
+        })
+        allP.forEach((el)=>{
+        if(Number(el.innerText) === random ){
+            el.classList.add('color')
+            }    
         })       
     }
 }
 
 button.addEventListener('click', randomF)
-
-const addCards = document.querySelector('section')
-const playerCard = document.getElementById('new-card')
-
-// const playerSelection = document.getElementById('player-card')
-// const value = playerSelection.value
-
-let arrCard = []
-
-const generateCard = function (){ 
-     for(let i=0; i<24; i++){
-        const cardCell = document.createElement('div')
-        const numCell = document.createElement('p')
-        cardCell.classList.add('card-cell')
-        cardCell.appendChild(numCell)
-     }
-     const divInSection = document.querySelectorAll('section div')
-     const newDiv = document.createElement('div')
-        divInSection.forEach((el)=>{
-        newDiv.appendChild(el)
-    })
-    newDiv.classList.add('new-card')
- }
-
-
- const blockDiv = function(){
-    const newNewDiv = document.querySelectorAll('section div')
-    newNewDiv.forEach((el)=>{
-        addCards.appendChild(el)
-    })
-}
-
-playerCard.addEventListener('click', function(e){
-    generateCard()
-    blockDiv()
-})
-
-
-
-
-
-
-
-
-
